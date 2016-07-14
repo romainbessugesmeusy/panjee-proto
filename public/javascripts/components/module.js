0e4117122001d32fcdd8299f178113722c3291e2
romainbessugesmeusy <romainbessuges@gmail.com>
2016-07-14 19:00:16 +0200

    Extend product template

--- public/javascripts/components/module.js
+++ public/javascripts/components/module.js
@@ -1,11 +1,12 @@
 var initModules = function (container, target) {
     var $debug = $('#debug');
+    var $modules = $(container).find('.panjeeModules');
 
     function getval(root, path) {
         var tmp = root, parts = ('' + path).split('.'), i = 0;
         for (; i < parts.length; i++) {
             if (typeof tmp[parts[i]] === 'undefined') {
-                return undefined;
+                return;
             }
             tmp = tmp[parts[i]];
         }
@@ -27,21 +28,39 @@ var initModules = function (container, target) {
         tmp[parts[parts.length - 1]] = value;
     }
 
+    function reset() {
+        $(container).find('.panjeeModule').each(function () {
+            valueDidChange($(this));
+        });
+    }
+
     $(container).find('.panjeeModule').each(function () {
         var $this = $(this);
         valueDidChange($this);
 
         $this.on('change', function (event) {
             var $input = $(event.target);
-            setval(target, $input.data('name'), $input.val());
+            setval(target, $input.attr('data-name'), $input.val());
+            $input.attr('data-value', $input.val());
             valueDidChange($this, $input, function () {
                 $debug.text(JSON.stringify(target, null, 2));
             });
         });
     });
 
-    function valueDidChange($module, $input, cb) {
+
+    function valueDidChange($module, $targetInput, cb) {
+
+        console.info('valueDidChange', $module[0]);
+
         cb = cb || new Function();
+
+        $module.find('[data-type=text] input').each(function(){
+            var $input = $(this);
+            if($input.attr('data-value')){
+                $input.val($input.attr('data-value'));
+            }
+        });
 
         $module.find('[data-choices]').each(function () {
             var $select = $(this);
@@ -61,7 +80,7 @@ var initModules = function (container, target) {
                 return cb();
             }
 
-            if ($select.data('path') !== resolved) {
+            if ($select.attr('data-path') !== resolved) {
                 $.getJSON('/api/entities/list/' + resolved, function (json) {
                     var options = ['<option>'];
                     if (json) {
@@ -69,18 +88,39 @@ var initModules = function (container, target) {
                             options.push($('<option>').attr({value: item.name}).text(item.label));
                         });
                     }
-                    $select.data('path', resolved);
+                    $select.attr('data-path', resolved);
                     $select.empty().append(options);
-                    if ($select.data('value')) {
-                        $select.val($select.data('value'));
+                    if ($select.attr('data-value')) {
+                        $select.val($select.attr('data-value'));
                     }
-                    setval(target, $select.data('name'), $select.val());
+                    setval(target, $select.attr('data-name'), $select.val());
                     return cb();
                 });
             } else {
+                if ($select.attr('data-value')) {
+                    $select.val($select.attr('data-value'));
+                }
                 cb();
             }
         });
+    }
+
+    function updateControls() {
+        $(container).find('[data-name]').each(function () {
+            var $control = $(this);
+            var data = $control.data();
+            console.info(target, data.name, getval(target, data.name));
+            $control.attr('data-value', getval(target, data.name));
+        });
+        reset();
+    }
+
+    return {
+        reset: reset,
+        modules: $modules,
+        updateControls: updateControls,
+        getval: getval,
+        setval: setval
     }
 };
 
--- public/javascripts/productForm.js
+++ public/javascripts/productForm.js
@@ -4,6 +4,7 @@ var formUrl = $('#formUrl').val();
 var formMethod = $('#formMethod').val();
 var jsonProduct = $('#jsonProduct').val();
 var organizationUrl = $('#organizationUrl').val();
+var organizationProducts = {};
 
 if (jsonProduct) {
     product = JSON.parse(jsonProduct);
@@ -26,13 +27,24 @@ $('button.save').click(function () {
 });
 
 $('#organizations-select').on('change', function () {
+    var $productSelect = $('#product-select').empty();
     $.getJSON('/api/organizations/' + $(this).val() + '/products', function (products) {
-        console.info(products);
+        $productSelect.append('<option>');
+        $(products).each(function (i, product) {
+            organizationProducts[product._id] = product.props;
+            $productSelect.append($('<option>').attr('value', product._id).text(product.name));
+        });
     });
+});
+
+$('#product-select').on('change', function () {
+    $.extend(product.props, organizationProducts[$(this).val()]);
+    modules.updateControls();
 });
 
 $('#productName').on('change', function () {
     product.name = $(this).val();
 });
 
-initModules($('#productForm'), product.props);
+
+var modules = initModules($('#productForm'), product.props);
